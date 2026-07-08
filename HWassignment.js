5d6c329bdeb3e272cffd4b9cdf97f817ce34e113
Angelselvaraj <angel.selvaraj@gmail.com>
2026-07-08 17:39:41 +0530

    Add files via upload
    week1 Day2 Assignment

---
+++ HWassignment.js
@@ -0,0 +1,22 @@
+var Name = "Angel"
+console.log(Name);
+console.log(typeof Name);
+var org = "TATA"
+console.log(org);
+console.log(typeof org);
+var contactNo = 9940374140
+console.log(contactNo);
+console.log(typeof contactNo);
+var knowsAutomation = false
+console.log(knowsAutomation);
+console.log(typeof knowsAutomation);
+var usesplaywright
+console.log(usesplaywight);
+console.log(typeof usesplaywright);
+
+
+
+
+
+
+
